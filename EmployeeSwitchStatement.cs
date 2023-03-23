df8ab1b1e5021b5c78e37df7a010b46f93982d9e
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-03-23 08:02:51 +0530

    Add files via upload

---
+++ EmployeeSwitchStatement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace EmployeeManagement
+{
+	public class EmployeeSwitchStatement
+	{
+		public static void SwitchStatement()
+		{
+			const int IS_FULL_TIME = 1;
+			const int IS_PART_TIME = 2;
+			const int EMP_RATE_PER_HR = 20;
+
+			int empHrs = 0;
+			int empWage = 0;
+			Random random = new Random();
+				int empCheck = random.Next(0, 3);
+			switch (empCheck)
+			{
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				default:
+					empHrs = 0;
+					break;
+
+			}
+			empWage = empHrs * EMP_RATE_PER_HR;
+			Console.WriteLine("Emp wage : " + empWage);
+		}
+	}
+}
+
