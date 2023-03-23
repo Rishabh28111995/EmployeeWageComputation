61319bd3c712d909e6297ce43bd71e27009da1c8
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-03-23 07:46:24 +0530

    Add files via upload

---
+++ ParttimeEmployee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace EmployeeManagement
+{
+    public class ParttimeEmployee
+    {
+        public static void ParttimeWage()
+        {
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_WAGE_PER_HR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME )
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_WAGE_PER_HR;
+            Console.WriteLine("Emp Wage :" + empWage);
+
+        }
+    }
+}
+
