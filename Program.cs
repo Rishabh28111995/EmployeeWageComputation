dc31baded347be71a3bdbdba770c11081ef78964
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-07-10 07:36:02 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,22 @@
-﻿namespace EmployeeeWage;
+namespace EmployeeeWage;
 class Program
 {
     public const int IS_PART_TIME = 1;
     public const int IS_FULL_TIME = 2;
     public const int Emp_Rate_hr = 20;
     public const int Num_Days = 20;
+    public const int Max_Working_Hrs_Month = 100;
 
     static void Main(string[] args)
     {
         int empHrs = 0;
-        int empWage = 0;
+        int totalEmpHrs = 0;
         int totalEmpWage = 0;
+        int totalWorkingDays = 0;
 
-        for (int day = 0; day < Num_Days; day++)
+        while (totalEmpHrs <= Max_Working_Hrs_Month && totalWorkingDays < Num_Days)
         {
+            totalWorkingDays++;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
             switch (empCheck)
@@ -28,11 +31,11 @@ class Program
                     empHrs = 0;
                     break;
             }
-            empWage = empHrs * Emp_Rate_hr;
-            totalEmpWage += empWage;
-            Console.WriteLine("Emp Wage : " + empWage);
-        }
+
+                    totalEmpWage += empHrs;
+                    Console.WriteLine("Day : " + totalWorkingDays + "Emp Hrs : " + empHrs);
+            }
+        totalEmpWage = totalEmpHrs * Emp_Rate_hr;
         Console.WriteLine("Total emp wage : " + totalEmpWage);
     }
 }
-
